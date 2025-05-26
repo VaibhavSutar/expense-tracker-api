f3c8fc4c468537668c31f259a5663de28b28455a
VaibhavSutar <sutarvaibhav37@gmail.com>
2025-05-26 08:30:38 -0600

    cron added

--- src/server.js
+++ src/server.js
@@ -3,15 +3,22 @@ import dotenv from 'dotenv';
 import { initDB } from './database.js'; // Adjust the path as necessary
 import rateLimiter from './middleware/rateLimiter.js';
 import transactionsRoute from "./routes/transactionsRoute.js"
+import jb from "./config/cron.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+if(process.env.NODE_ENV === "production"){
+    jb.start(); // Start the cron job
+}
 app.use(rateLimiter);
 app.use(express.json()); // Middleware to parse JSON bodies
 
 
-
+app.get("/api/health",(req,res)=>
+{
+    res.status(200).json({status:"ok"});
+})
 app.use('/api/transactions', transactionsRoute);
 
 
